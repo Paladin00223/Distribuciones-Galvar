5c298069afa9bba8aa2f20bb525c3ea3e8b25770
Paladin00223 <jdvargas223@gmail.com>
2025-06-19 18:45:22 -0500

    api

--- _app.py
+++ _app.py
@@ -1,6 +1,16 @@
-# Ejecuta este código en mcd
-# python -m pip install "pymongo[srv]"==3.12
 
+from flask import Flask, render_template,
+# Importamos la librería pymongo para con
+from pymongo import MongoClient
+# Importamos flask_cors para poder enviar
+from flask_cors import CORS #CORS: Inter
+
+#Creamo la aplicación con Flask
+app = Flask(__name__)
+
+#Habilitamos CORS en nuestra API
+CORS(app)
+#Conectamos la cadena de conexión a la BD
 
 
 from pymongo.mongo_client import MongoClient
@@ -14,3 +24,7 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+    
+# Ejecuta este código en mcd
+# python -m pip install "pymongo[srv]"==3.12
