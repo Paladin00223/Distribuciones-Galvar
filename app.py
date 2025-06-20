d6aae8f720e0c67715f50023083ac4320837d344
Paladin00223 <jdvargas223@gmail.com>
2025-06-19 21:15:37 -0500

    categorias

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
-#Importamos la librería Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+# Importamos la librería pymongo para con
+from pymongo import MongoClient
+# Importamos flask_cors para poder enviar
+from flask_cors import CORS #CORS: Inter
 
-#Creamos la aplicación con Flask
+#Creamo la aplicación con Flask
 app = Flask(__name__)
 
-#Definimos las rutas de la aplicación
+#Habilitamos CORS en nuestra API
+CORS(app)
+
+#Conectamos la cadena de conexión a la BD
+MONGO_URI = "mongodb+srv://jdvargas223:JDvargas223#@cluster0.u94fmoq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# COnectamos con la BD mongo mediante la
+mongo = MongoClient(MONGO_URI)
+# Seleccionamos la base de datos
+base_datos = mongo.distribuciones_galvar
+# Seleccionamos la colección
+coleccion_categorias = base_datos.categorias
+coleccion_productos = base_datos.productos
+
+# Definimos las rutas de la aplicación
 @app.route("/", methods=["GET"])
-def principal ():
+def principal():
+    #Consultamos las categorias en la coleccion
+    categorias = list(coleccion_categorias.find()) #find: listar todos
+    #Convertimos los ids a cadena de caracteres
+    for item in categorias:
+        item["_id"] = str(item["_id"])
 
-    # lógica de consulta a la Base de datos
-    # Se almacena en una variable los datos de la BD
-    # finalmente se envían al archivo html
+    # Enviamos la lista de videos al archivo html
+    return render_template("index.html", categorias=categorias)
 
-    return render_template("index.html", cuenta="Juan David Vargas")
+@app.route ("/products", methods=["GET"])
+def productos():
+    #Consultamos las categorias en la coleccion
+    productos = list(coleccion_productos.find()) #find: listar todos
+    #Convertimos los ids a cadena de caracteres
+    for item in productos:
+        item["_id"] = str(item["_id"])
 
-#Indicamos a python que esta es la aplicación principal
+    # Enviamos la lista de videos al archivo html
+    return render_template("products.html", productos=productos)
+
+
+
+
+# Ejecuta la aplicación principal
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+    
+# Ejecuta este código en mcd
+# pip install flask pymongo flask_cors
--- js/app.js
+++ js/app.js
@@ -1,30 +1,21 @@
-console.log('Hello World');
-
-
-
-
-
-
-
-
-
 
 //extraido de github
 
-const llenar_contenedor = (datos) => {
+const llenar_contenedor = (contenedor) => {
 
   let productos = '';
 
-  for (item of datos) {
+  for (item of productos) {
 
     let html = `
-    <article data-categoria="descechable">
-      <img src="${item.imagen}">
+    <article data-categoria="${item.categoria}">
+      <img src="${item.imagen.img1.igm}">
       <h3>${item.nombre}</h3>
+      <p>${item.describcion}
       <select class="precio">
-        <option value="unidad">Unidad</option>
+        <option value="unidad">${item.precios}</option>
       </select>
-      <b>Valor: <span>${item.unidad}</span></b><br>
+      <b>Valor: <span>${item.valor}</span></b><br>
       <label for="cant-C1"><b>Cantidad</b></label>
       <input id="cant-C1" name="cantidad" type="number" min="1" step="1" placeholder="Cantidad">
       <br>
